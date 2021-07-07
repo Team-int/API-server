91f07dbd1979da416693e25e2af0e4a1df1447a5
unknown <enter7377@naver.com>
2021-07-08 00:12:28 +0900

    Ffeat: directory

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,10 +1,8 @@
-const stockRoute = require('./stock')
-
 /**
  * @type {import('fastify').FastifyPluginCallback}
  */
 const routes = async (fastify, opts, done) => {
-    fastify.register(stockRoute, { prefix: '/stock' })
+    fastify.register(require('./stock/'), { prefix: '/stock' })
 
     done()
 }
--- src/routes/v1/stock/index.js
+++ src/routes/v1/stock/index.js
@@ -1,4 +1,4 @@
-const { db } = require('../../util/db')
+const { db } = require('../../../util/db')
 
 /**
  * @type {import('fastify').FastifyPluginCallback}
