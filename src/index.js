ab9e36fe0b25ecc4f2877ce4d25d3f72ff2d871f
안꿀꿀 <63600304+MadeGOD@users.noreply.github.com>
2021-07-08 20:25:22 +0800

    fix(ignore): 주소, 포트 커스텀 마이징 가능

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ const app = require('fastify').default({
 	logger: true,
 })
 
+let port = process.env.PORT || 3000
+let domain = process.env.DOMAIN || '0.0.0.0'
+
 db.connect().then(() => {
 	app.register(require('fastify-cors'))
 	app.register(require('./routes/v1'), {prefix: '/v1'})
 	app.register(require('./routes/v2'), {prefix: '/v2'})
 	
-	app.listen(4000, (err, addr) => {
+	app.listen(port, domain,(err, addr) => {
 		if (err)	throw err
 	  console.log(`[Team Int] Joined to API Server`, addr)
 	})
